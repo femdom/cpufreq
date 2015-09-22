0b7e91d90ecf2f2f7d629bfc62343d3bed0493d2
Renat Galimov <renat2017@gmail.com>
2015-09-23 00:53:55 +0300

    Made cpufreq as a default

--- build.rs
+++ build.rs
@@ -3,11 +3,11 @@ use std::process::Command;
 
 fn main() {
     let status = Command::new("ld")
-        .arg("-lcpufreq").status().unwrap();
+        .arg("-lcpupower").status().unwrap();
 
     if status.success() {
-        println!("cargo:rustc-link-lib=cpufreq");
-    } else {
         println!("cargo:rustc-link-lib=cpupower");
+    } else {
+        println!("cargo:rustc-link-lib=cpufreq");
     }
 }
